fc4db468e975e3ed0795d4c59e8b90cafc4f0c18
Rajiv Seelam <rajiv@betalectic.com>
2021-10-09 11:07:37 +0530

    Remove sample

--- src/stories/customers/UserCanViewCustomers/tests.spec.js
+++ src/stories/customers/UserCanViewCustomers/tests.spec.js
@@ -3,14 +3,7 @@ const debugLogger = requireUtil("debugLogger");
 describe("handler UserCanViewCustomers", () => {
   it("user_can_search_using_user_uuid", async () => {
     let result = {};
-    try {
-      result = await testStrategy("sampleStory", {
-        prepareResult: {},
-      });
-    } catch (error) {
-      debugLogger(error);
-    }
-    const { respondResult } = result;
+
     expect(1).toBe(1);
   });
 });
