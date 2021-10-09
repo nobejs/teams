6c2c0f62ada31bed137e05657f8fe9353ba9190b
Rajiv Seelam <rajiv@betalectic.com>
2021-10-09 11:12:13 +0530

    Fix indentation

--- src/stories/customers/UserCanViewCustomers/tests.spec.js
+++ src/stories/customers/UserCanViewCustomers/tests.spec.js
@@ -3,7 +3,6 @@ const debugLogger = requireUtil("debugLogger");
 describe("handler UserCanViewCustomers", () => {
   it("user_can_search_using_user_uuid", async () => {
     let result = {};
-
     expect(1).toBe(1);
   });
 });
