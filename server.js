9463e069cc7aa06472ab2ecf46a3f6152e32488c
Rajiv Seelam <rajiv@betalectic.com>
2021-10-11 23:49:20 +0530

    Update nobe

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Config = require("./config")();
 const httpServer = requireHttpServer();
 
 const server = httpServer({
-  logger: true,
+  logger: process.env.DEBUG === "true" ? true : false,
 });
 
 server.listen(process.env.PORT || 3000, "0.0.0.0", (err, address) => {
