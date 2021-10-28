9e848981b5ccaae7ac0868ce76684db4f60e2aae
Rajiv Seelam <rajiv@betalectic.com>
2021-10-28 16:27:16 +0530

    Add test to check for crypto lib

--- src/stories/Tokens/CanCreateTokenForTeam/tests.spec.js
+++ src/stories/Tokens/CanCreateTokenForTeam/tests.spec.js
@@ -1,16 +1,17 @@
 const debugLogger = requireUtil("debugLogger");
+const { createHash } = require("crypto");
 
 describe("Test Handler Tokens/CanCreateTokenForTeam", () => {
-  it("dummy_story_which_will_pass", async () => {
-    let result = {};
+  it("can_create_crypto_token", async () => {
+    let token = null;
     try {
-      result = await testStrategy("Tokens/CanCreateTokenForTeam", {
-        prepareResult: {},
-      });
+      const hash = createHash("sha256");
+      hash.update(`${Date.now()}`);
+      token = hash.digest("hex");
     } catch (error) {
       debugLogger(error);
     }
-    const { respondResult } = result;
-    expect(1).toBe(1);
+    console.log(":token", token);
+    expect(token).not.toBeNull();
   });
 });
